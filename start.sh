bd82b93821c1f2f45f119e0ffb70122424a95cf1
jcoleman <jtc331@gmail.com>
2018-12-26 20:21:30 +0000

    Auto-initdb on start.

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 # Choose an unused port on your machine
 export PGPORT=5444
 export PATH=$HOME/postgresql-test/bin:$PATH
+if [ ! -d "$HOME/postgresql-test-data" ]; then
+  initdb -D $HOME/postgresql-test-data
+fi
 pg_ctl -D $HOME/postgresql-test-data -l $HOME/postgresql-test-data.log start
