7c17b7697da75cb1be23c95374214c5ec512e090
jcoleman <jtc331@gmail.com>
2019-01-18 15:00:19 -0600

    Fix logging_collector=on sed command on macOS.
    According to https://stackoverflow.com/a/19457213 macOS's version of sed
    expects that -i will take an argument, so the previous form was treating
    the sed expression as the backup file extension. Specifying an empty
    extension to disable backups (and for good measure making the expression
    flag explicit) fixes the problem.

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ export PGPORT=5444
 export PATH=$HOME/postgresql-test/bin:$PATH
 if [ ! -d "$HOME/postgresql-test-data" ]; then
   initdb -D $HOME/postgresql-test-data
-  sed -i 's/#logging_collector = off/logging_collector = on\'$'\n#logging_collector = off/g' $HOME/postgresql-test-data/postgresql.conf
+  sed -i "" -e 's/#logging_collector = off/logging_collector = on\'$'\n#logging_collector = off/g' "$HOME/postgresql-test-data/postgresql.conf"
 fi
 pg_ctl -D $HOME/postgresql-test-data -l $HOME/postgresql-test-data.log start
