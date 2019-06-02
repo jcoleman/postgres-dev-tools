4ea9b403adbcf1dc79ace32bd864615af4fe92c3
James Coleman <jtc331@gmail.com>
2019-06-01 22:18:42 -0400

    Actually commit already-reference build config script.

---
+++ configure_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SOURCE_DIR="$HOME/postgres"
+if [ ! -d "$SOURCE_DIR" ]; then
+  SOURCE_DIR="$HOME/Source/postgres"
+fi
+
+pushd "$SOURCE_DIR"
+./configure --prefix=$HOME/postgresql-test --enable-cassert --enable-debug --enable-depend CFLAGS="-ggdb -Og -g3 -fno-omit-frame-pointer -DOPTIMIZER_DEBUG"
+popd
