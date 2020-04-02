80a72ac9cbd13efe96ec29ec788e4f3e48a0f51b
jcoleman <jtc331@gmail.com>
2020-04-02 13:08:50 +0000

    Minor variable cleanup.

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 # Choose an unused port on your machine
 export PGPORT=5444
-export PATH=$HOME/postgresql-test/bin:$PATH
+PG_PATH="$HOME/postgresql-test"
+PATH="$PG_PATH/bin:$PATH"
+
 if [ ! -d "$HOME/postgresql-test-data" ]; then
   initdb -D $HOME/postgresql-test-data
   sed -i "" -e 's/#logging_collector = off/logging_collector = on\'$'\n#logging_collector = off/g' "$HOME/postgresql-test-data/postgresql.conf"
--- stop.sh
+++ stop.sh
@@ -1,4 +1,6 @@
 # Choose an unused port on your machine
 export PGPORT=5444
-export PATH=$HOME/postgresql-test/bin:$PATH
+PG_PATH="$HOME/postgresql-test"
+PATH="$PG_PATH/bin:$PATH"
+
 pg_ctl -D $HOME/postgresql-test-data -l $HOME/postgresql-test-data.log stop
