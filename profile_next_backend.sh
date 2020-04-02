12fd2f8d2ee105fcaae836964afb5f3dd2066d7c
jcoleman <jtc331@gmail.com>
2020-04-02 13:10:13 +0000

    Add profile_next_backend.sh script.

---
+++ profile_next_backend.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Intended to be run from right pane:
+# - Run the script.
+# - Run psql or other connection to PG manually.
+# - Profiling will last the length of the backend.
+
+BACKEND_PID_CMD="ps aux --sort +etimes -C postgres | grep -v grep | grep postgres: | head -n1 | awk '{print \$2}'"
+
+OLD_PID="$(bash -c "$BACKEND_PID_CMD")"
+
+echo "Waiting for you to launch a backend now..."
+
+NEW_PID="$OLD_PID"
+
+while [ "$NEW_PID" = "$OLD_PID" ]; do
+  sleep 0.1
+  NEW_PID="$(bash -c "$BACKEND_PID_CMD")"
+done
+
+perf record -g -p $NEW_PID
