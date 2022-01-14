9b0550321be5b2da1fbec6ee49a8d380af6b03bc
jcoleman <jtc331@gmail.com>
2022-01-14 18:55:34 +0000

    Use lld for faster linking

--- configure_build.sh
+++ configure_build.sh
@@ -6,10 +6,12 @@ if [ ! -d "$SOURCE_DIR" ]; then
 fi
 
 pushd "$SOURCE_DIR"
+# Using lld for linking is apparently much faster than the default:
+# https://www.postgresql.org/message-id/CAH2-Wz=5MX0v9kEzSom9T7JNNW4AzPFFkCuY=e19KDP-=+z9Fg@mail.gmail.com
 ./configure --prefix=$HOME/postgresql-test \
   --enable-cassert \
   --enable-debug \
   --enable-depend \
   --enable-tap-tests \
-  CFLAGS="-ggdb -Og -g3 -fno-omit-frame-pointer -DOPTIMIZER_DEBUG"
+  CFLAGS="-ggdb -Og -g3 -fno-omit-frame-pointer -DOPTIMIZER_DEBUG -fuse-ld=lld"
 popd
