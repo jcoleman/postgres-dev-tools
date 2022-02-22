17609ca6255c9dca89cb9163035937191873c558
jcoleman <jtc331@gmail.com>
2022-02-22 21:03:01 +0000

    Fix logging collector enable sed script.

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ PATH="$PG_PATH/bin:$PATH"
 
 if [ ! -d "$HOME/postgresql-test-data" ]; then
   initdb -D $HOME/postgresql-test-data
-  sed -i "" -e 's/#logging_collector = off/logging_collector = on\'$'\n#logging_collector = off/g' "$HOME/postgresql-test-data/postgresql.conf"
+  newline=$'\n'
+  sed -i "" -e "s/#logging_collector = off/logging_collector = on${newline}#logging_collector = off/g" "$HOME/postgresql-test-data/postgresql.conf"
 fi
 pg_ctl -D $HOME/postgresql-test-data -l $HOME/postgresql-test-data.log start
